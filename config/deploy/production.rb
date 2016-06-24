7734e79970e6918b59bcfbac5d643c6a9686972e
Pankaj <nayal.pankaj@gmail.com>
2016-06-25 03:52:32 +0530

    deploy.rb & production.rb changes.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,6 +7,8 @@
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
+server '52.201.242.101', user: 'deploy', roles: %w{web app db}
+
 
 
 # role-based syntax
